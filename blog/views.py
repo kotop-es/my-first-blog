5180a8ced033f9de7a4930cc509a5034ac5d81c1
Admin <superkotopess@gmail.com>
2019-03-31 15:23:40 +0300

    Changed post sorting

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_new(request):
